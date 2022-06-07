49b024ecde026d85c7604119a8befa12a6383196
Aleksandr <aleksandrphilimonov@gmail.com>
2022-06-07 19:53:32 +0300

    add directory task2_2 and rename class Task2_2 to MainApp

--- src/com/gmail/aleksandrphilimonov/task2/task2_4/MainApp.java
+++ src/com/gmail/aleksandrphilimonov/task2/task2_4/MainApp.java
@@ -1,8 +1,8 @@
-package com.gmail.aleksandrphilimonov.task2;
+package com.gmail.aleksandrphilimonov.task2.task2_4;
 
 import java.util.Scanner;
 
-public class Task2_4 {
+public class MainApp {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
