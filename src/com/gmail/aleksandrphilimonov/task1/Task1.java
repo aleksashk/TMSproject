a0013a49305d92544555e2b6ae9bfcc592ced68e
Aleksandr <aleksandrphilimonov@gmail.com>
2022-06-08 16:29:42 +0300

    Add lesson 3 classwork

--- src/com/gmail/aleksandrphilimonov/task1/Task1.java
+++ src/com/gmail/aleksandrphilimonov/task1/Task1.java
@@ -2,6 +2,17 @@ package com.gmail.aleksandrphilimonov.task1;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("loop for -> i = " + i);
+        }
+        int j = 0;
+        while (j < 10){
+            System.out.println("loop while -> j = " + j++);
+        }
+
+        int k = 0;
+        do{
+            System.out.println("loop do/while -> k = " + k++);
+        }while (k < 10);
     }
 }
