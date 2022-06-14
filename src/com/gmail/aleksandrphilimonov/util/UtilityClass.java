bddf06dff81ce09a627cad179454a203453b5a9b
Aleksandr <aleksandrphilimonov@gmail.com>
2022-06-14 21:13:50 +0300

    Lesson 5 UtilityClass

--- src/com/gmail/aleksandrphilimonov/util/UtilityClass.java
+++ src/com/gmail/aleksandrphilimonov/util/UtilityClass.java
@@ -47,6 +47,111 @@ public class UtilityClass {
         return array;
     }
 
+    public static int sumOfMatrixElements(int[][] matrix) {
+        int result = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                result += matrix[i][j];
+            }
+        }
+        return result;
+    }
+
+    public static void matrixMultiplication(int[][] firstMatrix, int[][] secondMatrix) {
+        int m = firstMatrix.length;
+        int n = secondMatrix[0].length;
+        int o = secondMatrix.length;
+        int[][] res = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < o; k++) {
+                    res[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res[0].length; j++) {
+                System.out.format("%6d ", res[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] createTwoDimensionalMatrix() {
+        String msg = "Enter the first size of matrix: ";
+        UtilityClass.printer(msg);
+        int firstMatrixSize = getNumber();
+
+        msg = "Enter the second size of matrix: ";
+        UtilityClass.printer(msg);
+        int secondMatrixSize = getNumber();
+
+        msg = "Enter bound for filling array: ";
+        UtilityClass.printer(msg);
+        int bound = getNumber();
+
+        int[][] matrix = new int[firstMatrixSize][secondMatrixSize];
+        fillMatrix(matrix, bound);
+        return matrix;
+    }
+
+    private static void fillMatrix(int[][] matrix, int bound) {
+        Random random = new Random();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = random.nextInt(bound) - bound / 2;
+            }
+        }
+    }
+
+    public static int[][][] createThreeDimensionalMatrix() {
+        String msg = "enter first size for matrix: ";
+        printer(msg);
+        int firstSize = getNumber();
+
+        msg = "enter second size for matrix: ";
+        printer(msg);
+        int secondSize = getNumber();
+
+        msg = "enter third size for matrix: ";
+        printer(msg);
+        int thirdSize = getNumber();
+
+        msg = "Enter bound for filling array: ";
+        UtilityClass.printer(msg);
+        int bound = getNumber();
+
+        int[][][] matrix = new int[firstSize][secondSize][thirdSize];
+        fillMatrix(matrix, bound);
+
+        return matrix;
+    }
+
+    public static void enlargeElementInMatrix(int[][][] matrix, int number) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                for (int k = 0; k < matrix[i][j].length; k++) {
+                    matrix[i][j][k] += number;
+                }
+            }
+        }
+    }
+
+    private static void fillMatrix(int[][][] matrix, int bound) {
+        Random random = new Random();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                for (int k = 0; k < matrix[i][j].length; k++) {
+                    matrix[i][j][k] = random.nextInt(bound) - bound / 2;
+                }
+            }
+        }
+    }
+
     public static void printer(String msg) {
         System.out.print(msg);
     }
@@ -56,9 +161,23 @@ public class UtilityClass {
         System.out.println();
     }
 
+    public static void printer(int[][] matrix) {
+        System.out.print(Arrays.deepToString(matrix));
+        System.out.println();
+    }
+
     public static void printer(String[] array) {
         System.out.print(Arrays.toString(array));
         System.out.println();
+    }
+
+    public static void printer(int number) {
+        System.out.print(number);
+        System.out.println();
+    }
+
+    public static void print(int number) {
+        System.out.print(number);
     }
 
     public static void quickSort(int[] array, int low, int high) {
@@ -151,6 +270,50 @@ public class UtilityClass {
         for (int i = 0; i < array.length; i++) {
             printer(msg);
             array[i] = scanner.nextLine();
+        }
+    }
+
+    public static void printSecondDiagonal(int[][] array) {
+        String msg;
+        msg = "\nSecond diagonal: ";
+        printer(msg);
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i].length - i - 1 == j) {
+                    print(array[j][i]);
+                }
+            }
+        }
+    }
+
+    public static void printMainDiagonal(int[][] array) {
+        String msg = "Main diagonal: ";
+        printer(msg);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (i == j) {
+                    print(array[i][j]);
+                }
+            }
+        }
+    }
+
+    public static void sortMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                for (int k = j + 1; k < matrix.length; k++) {
+                    swapInMatrix(matrix, i, j, k);
+                }
+            }
+        }
+    }
+
+    public static void swapInMatrix(int[][] matrix, int i, int j, int k) {
+        if (matrix[i][j] > matrix[i][k]) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][k];
+            matrix[i][k] = temp;
         }
     }
 }
