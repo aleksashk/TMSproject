59557a18e6f49da85b536a543aa5ebb0262c74f4
Aleksandr <aleksandrphilimonov@gmail.com>
2022-06-30 20:59:27 +0300

    lesson 10 task1 show report does string contains "abc"

--- src/com/gmail/aleksandrphilimonov/lesson10/classwork/MainApp.java
+++ src/com/gmail/aleksandrphilimonov/lesson10/classwork/MainApp.java
@@ -8,6 +8,8 @@ public class MainApp {
 //        Scanner scanner = new Scanner(System.in);
 //        String string = scanner.nextLine();
         String string = "1234-dpm-2345-xyz-9r7w";
+        String string2 = "1234-dpm-2345-abc-9r7w";
+        String string3 = "1234-ABC-2345-ert-9r7w";
         String delimiter = "\n--------------------------------\n";
         StringUtil.printFirstTwoNumericBlocks(string);
         UtilityClass.printer(delimiter);
@@ -16,6 +18,11 @@ public class MainApp {
         UtilityClass.printer(delimiter);
 
         StringUtil.showOnlyLetters(string);
+        UtilityClass.printer(delimiter);
+
+        StringUtil.isContainsAbc(string);
+        StringUtil.isContainsAbc(string2);
+        StringUtil.isContainsAbc(string3);
         UtilityClass.printer(delimiter);
     }
 }
--- src/com/gmail/aleksandrphilimonov/lesson10/classwork/string_util_package/StringUtil.java
+++ src/com/gmail/aleksandrphilimonov/lesson10/classwork/string_util_package/StringUtil.java
@@ -2,6 +2,8 @@ package com.gmail.aleksandrphilimonov.lesson10.classwork.string_util_package;
 
 import com.gmail.aleksandrphilimonov.util.UtilityClass;
 
+import java.util.Arrays;
+
 public class StringUtil {
     public static void printFirstTwoNumericBlocks(String string) {
         String delimiter = "-";
@@ -21,7 +23,7 @@ public class StringUtil {
         String delimiter = "-";
         String stringForReplace = "***";
         StringBuilder sb = new StringBuilder();
-        String[] array = string.split(delimiter);
+        String[] array = splitStringByDash(string);
         for (String s : array) {
             isThreeLetters(delimiter, stringForReplace, sb, s);
         }
@@ -29,9 +31,9 @@ public class StringUtil {
         System.out.print(sb);
     }
 
-    public static void showOnlyLetters(String string){
+    public static void showOnlyLetters(String string) {
         StringBuilder sb = new StringBuilder();
-        String[] sArray = string.split("-");
+        String[] sArray = splitStringByDash(string);
         sb.append(sArray[1].toLowerCase())
                 .append('/')
                 .append(sArray[3].toLowerCase())
@@ -39,7 +41,23 @@ public class StringUtil {
         sb.append(sArray[4].toLowerCase().charAt(1))
                 .append("/")
                 .append(sArray[4].toLowerCase().charAt(3));
-        System.out.print(sb);
+        UtilityClass.printer(sb.toString());
+    }
+
+    public static void isContainsAbc(String string) {
+        String checkString = "abc";
+        String msg = "";
+        String[] sArray = string.split("-");
+        for (int i = 0; i < sArray.length; i++) {
+            String s = Arrays.toString(sArray).toLowerCase();
+            if (s.contains(checkString)) {
+                msg = "sequence " + s + " contains string " + checkString + "\n";
+                break;
+            } else {
+                msg = "sequence " + s + " don't contains string " + checkString + "\n";
+            }
+        }
+        UtilityClass.printer(msg);
     }
 
     private static void isThreeLetters(String delimiter, String stringForReplace, StringBuilder sb, String s) {
