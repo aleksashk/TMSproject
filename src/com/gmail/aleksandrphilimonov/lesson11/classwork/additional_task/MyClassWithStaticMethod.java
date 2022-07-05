f47fba7ce10ce4e7f61acd0bfe35332fba58de55
Aleksandr <aleksandrphilimonov@gmail.com>
2022-07-05 21:25:12 +0300

    changed additional task

--- src/com/gmail/aleksandrphilimonov/lesson11/classwork/additional_task/MyClassWithStaticMethod.java
+++ src/com/gmail/aleksandrphilimonov/lesson11/classwork/additional_task/MyClassWithStaticMethod.java
@@ -2,22 +2,25 @@ package com.gmail.aleksandrphilimonov.lesson11.classwork.additional_task;
 
 public class MyClassWithStaticMethod {
     public static boolean UserVerification(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        boolean result = true;
-        if(login.length() >= 20 || login.contains(" ")){
-            result = false;
+        boolean result = checkLoginData(login);
+
+        if (login.contains(" ")) {
             throw new WrongLoginException("Incorrect login.");
         }
-        if(password.length() >= 20 || password.contains(" ") || !containsDigit(password) || !password.equals(confirmPassword)){
-            result = false;
+        if (password.length() >= 20 || password.contains(" ") || !containsDigit(password) || !password.equals(confirmPassword)) {
             throw new WrongPasswordException("Incorrect password.");
         }
         return result;
     }
 
+    private static boolean checkLoginData(String login) {
+        return login.length() >= 20;
+    }
+
     private static boolean containsDigit(String password) {
         char[] chars = password.toCharArray();
         for (char ch : chars) {
-            if(Character.isDigit(ch)){
+            if (Character.isDigit(ch)) {
                 return true;
             }
         }
